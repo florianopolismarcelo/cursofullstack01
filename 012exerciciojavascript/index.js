ca99e33916d80ce8bc2732d6c747a93776d32e41
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-08-27 09:55:16 -0300

    javascript diferença de idade

---
+++ 012exerciciojavascript/index.js
@@ -0,0 +1,20 @@
+let olderPersonName = prompt("Digite o nome da pessoa mais velha")
+let olderPersonAge = prompt("Digite a idade da pessoa mais velha")
+
+let youngerPersonName = prompt("Digite o nome da pessoa mais nova")
+let youngerPersonAge = prompt("Digite a idade da pessoa mais nova")
+
+let ageDiference = olderPersonAge - youngerPersonAge
+
+alert(
+  "Pessoa mais velha: " +
+    olderPersonName +
+    "\nIdade: " +
+    olderPersonAge +
+    "\n\nPessoa mais nova: " +
+    youngerPersonName +
+    "\nIdade: " +
+    youngerPersonAge +
+    "\n\nDifereça de idade: " +
+    ageDiference
+)
