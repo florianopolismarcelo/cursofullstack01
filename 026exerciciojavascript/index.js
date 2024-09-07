2ea9ddf9ecd3a94dfb998850722bed5bfc87df3b
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-09-07 13:29:13 -0300

    javascript enviando o dom para a função

---
+++ 026exerciciojavascript/index.js
@@ -0,0 +1,7 @@
+function sayMyFirstName(element){
+  alert("Meu primeiro nome é " + element.value)
+}
+function sayMyLastName(){
+  console.log(event)
+  alert("Meu últino nome é " + event.target.value)
+}
